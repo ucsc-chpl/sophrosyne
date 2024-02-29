e1b10e6016b4c2b9605e8a0a6cc132b51de1a7c8
Sean Siddens <seansiddens@gmail.com>
2024-02-28 22:07:21 -0800

    Running discovery for multiple trials

--- src/benchmarks/occupancy_discovery.rs
+++ src/benchmarks/occupancy_discovery.rs
@@ -10,21 +10,36 @@ pub async fn occupancy_discovery() {
     let num_workgroups = 1024;
     let workgroup_size = 1;
 
-    // Define host buffers.
-    let mut h_count = vec![0u32; 1];
-    let mut h_poll = vec![0u32; 1];
-    let mut h_scratchpad = vec![0u32; 10_000];
+    let n = 32;
+    let mut occupancy_counts = vec![0u32; n];
 
-    device
-        .launch_compute(
-            "../shaders/wgsl/occupancy_discovery.wgsl",
-            &mut vec![&mut h_count, &mut h_poll, &mut h_scratchpad],
-            num_workgroups,
-            workgroup_size,
-        )
-        .await;
+    for i in 0..n {
+        // Define host buffers.
+        let mut h_count = vec![0u32; 1];
+        let mut h_poll = vec![0u32; 1];
+        let mut h_scratchpad = vec![0u32; 10_000];
 
-    // Print results.
-    // Results are automatically mapped back to the host.
-    println!("Count: {}", h_count[0]);
+        device
+            .launch_compute(
+                "../shaders/wgsl/occupancy_discovery.wgsl",
+                &mut vec![&mut h_count, &mut h_poll, &mut h_scratchpad],
+                num_workgroups,
+                workgroup_size,
+            )
+            .await;
+
+        // Print results.
+        // Results are automatically mapped back to the host.
+        println!("Count: {}", h_count[0]);
+        occupancy_counts[i] = h_count[0];
+    }
+
+    // Compute average occupancy.
+    let sum: u32 = occupancy_counts.iter().sum();
+    let average_occupancy = sum / n as u32;
+    println!("Average occupancy: {}", average_occupancy);
+
+    // Compute maximum occupancy.
+    let max_occupancy = *occupancy_counts.iter().max().unwrap();
+    println!("Maximum occupancy: {}", max_occupancy);
 }
