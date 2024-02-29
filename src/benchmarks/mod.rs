218ec58dfc7ab1148d129c32a16548a1ecc127e3
Sean Siddens <seansiddens@gmail.com>
2024-02-28 21:43:01 -0800

    format

--- src/benchmarks/mod.rs
+++ src/benchmarks/mod.rs
@@ -1,7 +1,7 @@
-mod vector_add;
 mod occupancy_discovery;
+mod vector_add;
 
 // Re-export benchmark functions directly.
+pub use occupancy_discovery::occupancy_discovery;
 #[allow(unused_imports)]
 pub use vector_add::vector_add;
-pub use occupancy_discovery::occupancy_discovery;
