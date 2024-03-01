cda6a6aa473c22ef96dfc0e0e17b57ebac0e11e0
Sean Siddens <seansiddens@gmail.com>
2024-02-29 17:26:55 -0800

    Starting to process commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,61 @@
 mod benchmarks;
 
+use clap::Parser;
+use std::process;
+use wgpu::AdapterInfo;
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Name of benchmark to run.
+    #[arg()]
+    benchmark: Option<String>,
+
+    /// Device to run benchmark on.
+    #[arg(short, long)]
+    device: Option<usize>,
+
+    /// Enumerate all available devices.
+    #[arg(short, long)]
+    list_devices: bool,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    if args.list_devices {
+        let device_info = easywg::list_devices();
+        for (i, info) in device_info.iter().enumerate() {
+            println!("Adapter {}: {:#?}", i, info);
+        }
+        return;
+    } else {
+        match args.device {
+            Some(device) => {
+                println!("Device: {}", device);
+                match args.benchmark.as_deref() {
+                    Some("occupancy_discovery") => {
+                        pollster::block_on(benchmarks::occupancy_discovery());
+                    }
+                    Some("vector_add") => {
+                        pollster::block_on(benchmarks::vector_add());
+                    }
+                    Some("atomic_throughput") => {
+                        pollster::block_on(benchmarks::atomic_throughput());
+                    }
+                    _ => {
+                        eprintln!("No benchmark specified.");
+                        process::exit(1);
+                    }
+                }
+            }
+            None => {
+                eprintln!("No device specified.");
+                process::exit(1);
+            }
+        }
+    }
+
     // Creates a logger, filtering out all log messages except those from this module.
     env_logger::builder()
         .filter_level(log::LevelFilter::Off)
@@ -11,5 +66,5 @@ fn main() {
 
     // Needs to be run in an async environment.
     // Pollster provides a simple way to do this (should work on all platforms).
-    pollster::block_on(benchmarks::vector_add());
+    pollster::block_on(benchmarks::occupancy_discovery());
 }
