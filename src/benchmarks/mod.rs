a48e548f61a6c0f2c551a39113c3f26710c5656b
Sean Siddens <seansiddens@gmail.com>
2024-02-29 17:26:20 -0800

    removed ignore unused code directive

--- src/benchmarks/mod.rs
+++ src/benchmarks/mod.rs
@@ -1,7 +1,8 @@
+mod atomic_throughput;
 mod occupancy_discovery;
 mod vector_add;
 
 // Re-export benchmark functions directly.
+pub use atomic_throughput::atomic_throughput;
 pub use occupancy_discovery::occupancy_discovery;
-#[allow(unused_imports)]
 pub use vector_add::vector_add;
