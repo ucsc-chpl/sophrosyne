27f2b42d0e4cc61ba97110874e95f7bcec9c3a88
Sean Siddens <seansiddens@gmail.com>
2024-02-29 17:27:21 -0800

    Initial commit

---
+++ src/benchmarks/atomic_throughput.rs
@@ -0,0 +1,30 @@
+use easywg::*;
+
+pub async fn atomic_throughput() {
+    let device = Device::new().await;
+
+    log::info!("Device name: {:?}", device.adapter_info.name);
+    log::info!("Using backend: {:?}", device.adapter_info.backend);
+
+    let n: usize = 16;
+
+    // Define host buffers.
+    let mut h_a = vec![1u32; n];
+    let mut h_b = vec![1u32; n];
+    let mut h_c = vec![0u32; n];
+
+    device
+        .launch_compute(
+            "../shaders/wgsl/atomic_throughput.wgsl",
+            &mut vec![&mut h_a, &mut h_b, &mut h_c],
+            n as u32,
+            1,
+        )
+        .await;
+
+    // Print results.
+    // Results are automatically mapped back to the host.
+    for i in 0..n {
+        log::info!("{} + {} = {}", h_a[i], h_b[i], h_c[i]);
+    }
+}
