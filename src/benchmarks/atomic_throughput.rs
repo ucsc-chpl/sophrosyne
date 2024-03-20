1e23e7a66b757c3546a6102f14b704dd306b660e
Sean Siddens <seansiddens@gmail.com>
2024-03-19 19:44:11 -0700

    Basic atomic throughput contiguous benchmark

--- src/benchmarks/atomic_throughput.rs
+++ src/benchmarks/atomic_throughput.rs
@@ -7,24 +7,34 @@ pub async fn atomic_throughput() {
     log::info!("Using backend: {:?}", device.adapter_info.backend);
 
     let n: usize = 16;
+    let test_iters = 64;
+    let rmw_iters = 16;
+
+    let workgroup_size = 4;
+    // TODO: Use occupancy discovery to get # of workgroups.
+    let workgroups = 3;
+    let contention = 1;
+    let padding = 1;
 
     // Define host buffers.
-    let mut h_a = vec![1u32; n];
-    let mut h_b = vec![1u32; n];
-    let mut h_c = vec![0u32; n];
-
-    device
-        .launch_compute(
-            "../shaders/wgsl/atomic_throughput.wgsl",
-            &mut vec![&mut h_a, &mut h_b, &mut h_c],
-            n as u32,
-            1,
-        )
-        .await;
-
-    // Print results.
-    // Results are automatically mapped back to the host.
-    for i in 0..n {
-        log::info!("{} + {} = {}", h_a[i], h_b[i], h_c[i]);
+    let mut h_res = vec![0u32; workgroups * workgroup_size];
+    let mut h_iters = vec![16u32; 1];
+    h_iters[0] = rmw_iters;
+    let mut h_indexes = vec![0u32; workgroups * workgroup_size];
+    for i in 0..workgroups * workgroup_size {
+        // Contiguous access.
+        h_indexes[i] = ((i / contention) * padding) as u32;
     }
+
+    for _ in 0..test_iters {
+        device
+            .launch_compute(
+                "shaders/wgsl/atomic_fa_relaxed.wgsl",
+                &mut vec![&mut h_res, &mut h_iters, &mut h_indexes],
+                n as u32,
+                1,
+            )
+            .await;
+    }
+
 }
